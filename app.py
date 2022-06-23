92d80d1caf4580b980236afabeaa7d62ccc5b679
MoohamedKhaled <mohamedrobot1@gmail.com>
2022-06-23 15:02:15 +0200

    fix private key

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ cred = credentials.Certificate(
         "type":os.environ['type'],
         "project_id": os.environ['project_id'],
         "private_key_id": os.environ['private_key_id'],
-        "private_key": os.environ['private_key'],
+        "private_key": os.environ['private_key'].replace("\\n", "\n"),
         "client_email": os.environ['client_email'],
         "client_id": os.environ['client_id'],
         "auth_uri": os.environ['auth_uri'],
